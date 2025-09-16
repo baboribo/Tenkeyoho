1de088c05e39fb72e07d162bf373a8dd66538b87
Baboribo <inertia_mailer_0c@icloud.com>
2025-09-16 06:39:59 +0000

    feat: map 임시사용

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,13 @@ function App() {
 
     useEffect(() => {
         Promise.all([
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=danwon-gu&appid=${API_KEY}&units=metric&lang=kr`),
-            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=danwon-gu&appid=${API_KEY}&units=metric&lang=kr`),
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=danwon-gu&appid=${API_KEY}&units=metric&lang=kr`), // 1 현재 날씨
+            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=danwon-gu&appid=${API_KEY}&units=metric&lang=kr`), // 2 예보
         ])
             .then(([currentRes, forecastRes]) => {
                 setTenki(currentRes.data);
                 setForecast(forecastRes.data);
                 setLoading(false);
-                const forecastlist = forecastRes.data.list;
             })
             .catch((error) => {
                 console.error("날씨 정보를 불러오지 못했습니다:", error);
@@ -33,6 +32,11 @@ function App() {
             <h1>{tenki.name}</h1>
             <h2>온도: {tenki.main.temp}°C</h2>
             <p>날씨 상태: {tenki.weather[0].description}</p>
+            {forecast.map((item, index) => (
+                <span key={index}>
+                    {item.dt_txt} - {item.main.temp}°C - {item.weather[0].description}
+                </span>
+            ))}
         </div>
     );
 }
