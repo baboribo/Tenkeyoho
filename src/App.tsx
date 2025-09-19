9bca738574235d60e709c1e4123f4a782b3627e1
Baboribo <inertia_mailer_0c@icloud.com>
2025-09-19 04:54:42 +0000

    style: App.tsx에 Motion 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,31 @@ import { useState, useEffect } from 'react';
 import useEmblaCarousel from "embla-carousel-react";
 import axios from 'axios';
 import { motion } from "motion/react"
+import type { exit } from 'process';
+import { animate, delay } from 'motion';
 
 function App() {
     const [emblaRef] = useEmblaCarousel({dragFree: true});
     const [tenki, setTenki] = useState(null);
     const [forecast, setForecast] = useState(null);
     const [loading, setLoading] = useState(true);
+    const query = 'danwon-gu';
     const transition = {
-        duration: 0.2,
-        stiffness: 1185,
-        dumping: 100,
-        mass: 1,
+        duration: 0.42,
+        ease: [.17,.02,.05,.98],
+    }
+    const transition2 = {
+        duration: 0.32,
+        delay: 0.27,
+        ease: [.17,.02,.05,.98],
     }
 
     const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
     useEffect(() => {
         Promise.all([
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=${API_KEY}&units=metric&lang=kr`), // 1 현재 날씨
-            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=seoul&appid=${API_KEY}&units=metric&lang=kr`), // 2 예보
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric&lang=kr`), // 1 현재 날씨
+            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=metric&lang=kr`), // 2 예보
         ])
             .then(([currentRes, forecastRes]) => {
                 setTenki(currentRes.data);
@@ -33,7 +39,7 @@ function App() {
             });
     }, []);
 
-    if (loading) return <p>로딩 중...</p>;
+    if (loading) return <motion.p transition={transition} initial={{ opacity: 0, y: 40, x: 40, scale: 0.9 }} animate={{ opacity: 1, y: 20, scale: 1 }} exit={{ opacity: 0 }}>로딩 중...</motion.p>;
     if (!tenki && !forecast) return <p>날씨 정보를 불러오지 못했습니다.</p>;
 
     return (
@@ -50,7 +56,7 @@ function App() {
                             <h1 className="font-bold">{tenki.weather[0].description}</h1>
                             <h3 className="text-3xl">{tenki.main.temp}°C</h3>
                         </div>
-                        <img className="w-30 h-30" src={`https://openweathermap.org/img/wn/${tenki.weather[0].icon}@2x.png`} alt="weather icon"/>
+                        <motion.img transition={transition2} initial={{opacity: 0}} animate={{opacity: 1}} className="w-30 h-30" src={`https://openweathermap.org/img/wn/${tenki.weather[0].icon}@2x.png`} alt="weather icon"/>
                     </div>
                     <ul className="flex gap-6">
                         <li className="flex flex-col">
@@ -75,7 +81,7 @@ function App() {
                         </li>
                     </ul>
                 </section>
-                <section className="embla" ref={emblaRef}>
+                <motion.section transition={transition2} initial={{opacity: 0, y: 60}} animate={{opacity: 1, y: 0}} className="embla" ref={emblaRef}>
                     <div className="item">
                         {forecast.list.map((item, index) => (
                             <span key={index}>
@@ -86,7 +92,7 @@ function App() {
                             </li>
                             <li className="text-xl">{item.main.temp}°C</li>
                             <li>
-                                <img className="w-20 h-20" src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="weather icon"/>
+                                <motion.img transition={transition2} initial={{opacity: 0}} animate={{opacity: 1}} className="w-20 h-20" src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="weather icon"/>
                                  <p>체감 온도</p>
                                  <h4 className="text-lg">{item.main.feels_like}°C</h4>
                             </li>
@@ -94,7 +100,7 @@ function App() {
                     </span>
                         ))}
                     </div>
-                </section>
+                </motion.section>
             </main>
         </motion.body>
 
