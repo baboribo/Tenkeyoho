2ae5f33be07fe3798cea0671ceaa2e1cf980d001
Baboribo <inertia_mailer_0c@icloud.com>
2025-10-05 10:25:52 +0000

    style: 메인 사이트와 스타일 일부 통합 및 GNB 상좌우에 고정 되도록 고침

---
+++ src/components/gnb.tsx
@@ -0,0 +1,34 @@
+import { SiGithub } from '@icons-pack/react-simple-icons';
+
+export default function Gnb() {
+  return (
+    <div className="fixed backdrop-blur-md top-0 left-0 right-0 transition">
+      <div className='flex pt-2 pb-2 pl-6 pr-6 w-full justify-between items-center'>
+        <div className='flex gap-4 items-center'>
+          <img className="w-8.5 h-8.5 rounded-4xl" src="https://avatars.githubusercontent.com/u/71020988?v=4" alt="profile"/>
+          <div className='flex gap-2 items-center'>
+            <h4>m_ny29</h4>
+            <p>/</p>
+            <h4>Tenki</h4>
+          </div>
+          {/* <div className='flex'>
+            <a href='/' className='pl-3 pr-3 pt-1 pb-1 rounded-4xl hover:bg-blend-overlay hover:bg-gray-500 hover:blend-multiply transition' >
+              <p>홈</p>
+            </a>
+            <a href='/about' className='pl-3 pr-3 pt-1 pb-1 rounded-4xl hover:bg-blend-overlay hover:bg-gray-500 hover:blend-multiply transition' >
+              <p>소개</p>
+            </a>
+          </div> */}
+        </div>
+        <div className='flex gap-4 items-center'>
+            <a href='https://yoru.yume.place'>
+                <p className='opacity-70 hover:opacity-100 transition cursor-pointer'>밤의 세계로 이동</p>
+            </a>
+            <a href='https://github.com/baboribo/Tenkeyoho'>
+                <SiGithub size={24} color="#ffffff" className='opacity-70 hover:opacity-100 transition cursor-pointer' />
+            </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Gnb from './components/gnb.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='flex pt-3 pb-3 pl-5 pr-5 w-full fixed'>
-            <div className='flex gap-4 items-center'>
-                    <img className="w-9 h-9 rounded-4xl" src="/Im-just-watching.jpg" alt="profile"/>
-                    <div className='flex gap-2 items-center'>
-                        <h4>mny29</h4>
-                        <p>/</p>
-                        <h4>Tenki</h4>
-                    </div>
-                   
-                    <div className='flex'>
-                           <a href='https://github.com/baboribo' className='pl-3 pr-3 pt-1 pb-1 rounded-4xl hover:bg-blend-overlay hover:bg-gray-500 hover:blend-multiply transition' >
-                              <p>깃허브</p>
-                          </a>
-                          <a href='https://github.com/baboribo' className='pl-3 pr-3 pt-1 pb-1 rounded-4xl hover:bg-blend-overlay hover:bg-gray-500 hover:blend-multiply transition' >
-                              <p>소개</p>
-                           </a>
-                    </div>
-            </div>
-    </div>
+    <Gnb />
     <App />
   </StrictMode>,
 )
